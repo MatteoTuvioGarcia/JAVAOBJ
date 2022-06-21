a4709e84a4adcba98409e72e47f3958aca4fe58b
Tuvio717 <46975220+MatteoTuvioGarcia@users.noreply.github.com>
2022-06-21 16:26:09 +0200

    Working beer form

--- src/main/java/fr/almeri/beerboard/controllers/beersController.java
+++ src/main/java/fr/almeri/beerboard/controllers/beersController.java
@@ -6,9 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 //indique à spring boot un routage
 @Controller
@@ -17,6 +18,7 @@ public class beersController {
 
     @Autowired
     private BiereRepository biereRepository;
+    private MarqueRepository marqueRepository;
 
     @GetMapping("/beers")
     public String getPageExemple(Model pModel)
@@ -27,8 +29,14 @@ public class beersController {
         return "beers";
     }
 
-    @GetMapping("/see-beer/{marque}?{version}")
-    public String getBrewery(Model pModel, @PathVariable(required = true) String marque, @PathVariable(required = true) String version){
+    @GetMapping("/see-beer")
+    public String GetBeerByCode(Model pModel, @RequestParam String nomMarque, @RequestParam String nomVersion )
+    {
+        BiereId bid = new BiereId(new Marque(nomMarque),nomVersion);
+        Biere beer = biereRepository.findById(bid).orElseThrow();
+        pModel.addAttribute("biere", beer);
+        pModel.addAttribute("marque",bid.getMarque());
+
 
         return "beer";
     }
--- src/main/java/fr/almeri/beerboard/repositories/BiereRepository.java
+++ src/main/java/fr/almeri/beerboard/repositories/BiereRepository.java
@@ -4,12 +4,14 @@ import fr.almeri.beerboard.models.Biere;
 import fr.almeri.beerboard.models.BiereId;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @Repository
-public interface BiereRepository extends CrudRepository<Biere, String> {
+public interface BiereRepository extends CrudRepository<Biere, BiereId> {
     @Query("SELECT distinct p.tauxAlcool FROM Biere p group by p.tauxAlcool ORDER BY p.tauxAlcool ASC")
     public ArrayList<String> getThxAlcoolAsc();
     @Query("SELECT COUNT(p.version) FROM Biere p group by p.tauxAlcool ORDER BY p.tauxAlcool ASC")
@@ -20,4 +22,7 @@ public interface BiereRepository extends CrudRepository<Biere, String> {
 
     @Query("SELECT count(p.version) FROM Biere p group by p.marque.nomMarque ORDER BY p.marque.nomMarque ASC")
     public int[] getNbrVerparmarqueAlcoolAsc();
+
+    @Query("SELECT p FROM Biere p where p.marque.brasserie.codeBrasserie = :code order by p.marque.nomMarque asc")
+    public ArrayList<Biere> getBierefromBrasserie(@Param("code") String code);
 }
